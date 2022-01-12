db6e4eb3e00c84d385d4bb70794a7caac0f7665a
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-01-12 22:53:41 +0200

    fixing linters

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ class Book {
 class LocalStoragConfig {
   static getBookFromStorage() {
     let books;
-    if (localStorage.getItem("books") === null) {
+    if (localStorage.getItem('books') === null) {
       books = [];
     } else {
-      books = JSON.parse(localStorage.getItem("books"));
+      books = JSON.parse(localStorage.getItem('books'));
     }
     return books;
   }
@@ -21,21 +21,21 @@ class LocalStoragConfig {
   static AddBook(book) {
     const books = LocalStoragConfig.getBookFromStorage();
     books.push(book);
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
 
   static RemoveStoring(title) {
     const books = LocalStoragConfig.getBookFromStorage();
     const filteredArray = books.filter((book) => book.title !== title);
-    localStorage.setItem("books", JSON.stringify(filteredArray));
+    localStorage.setItem('books', JSON.stringify(filteredArray));
   }
 }
 
 class BookElements {
   static BookElement(book) {
-    const bookContainer = document.querySelector(".book-container");
-    const listContainer = document.createElement("div");
-    listContainer.className = "list-Container";
+    const bookContainer = document.querySelector('.book-container');
+    const listContainer = document.createElement('div');
+    listContainer.className = 'list-Container';
     listContainer.innerHTML += `
             <p class="text">"${book.title}" by ${book.author}</p>
             <button class='delete'>Remove</button>
@@ -52,18 +52,18 @@ class Display {
   }
 
   static removeBook(target) {
-    if (target.classList.contains("delete")) {
+    if (target.classList.contains('delete')) {
       target.parentElement.remove();
     }
   }
 }
 
-document.addEventListener("DOMContentLoaded", Display.displayBooksFromStorage);
-document.querySelector("#form").addEventListener("submit", (e) => {
+document.addEventListener('DOMContentLoaded', Display.displayBooksFromStorage);
+document.querySelector('#form').addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const title = document.getElementById("title");
-  const author = document.getElementById("author");
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
 
   const book = new Book(title, author);
 
@@ -71,14 +71,14 @@ document.querySelector("#form").addEventListener("submit", (e) => {
 
   BookElements.BookElement(book);
 
-  const form = document.querySelector("#form");
+  const form = document.querySelector('#form');
   form.reset();
 });
 
-document.querySelector(".book-container").addEventListener("click", (e) => {
+document.querySelector('.book-container').addEventListener('click', (e) => {
   Display.removeBook(e.target);
 
   LocalStoragConfig.RemoveStoring(
-    e.target.parentElement.firstElementChild.textContent
+    e.target.parentElement.firstElementChild.textContent,
   );
 });
